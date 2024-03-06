0b5efe092fc1ac77db0029ae28278ef98dd7c454
Christians IdeaPad <cs.christian.steiner@gmail.com>
2024-03-06 09:04:06 +0100

    added SPA components, added +layout.js and +layout.svelte for navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<nav>
+	<ul>
+		<li><a href ="/">Suedstadt</a></li>
+		<li><a href ="/history">Geschichte</a></li>
+		<li><a href ="/teams">Teams</a></li>
+		<li><a href ="/calender">Kalender</a></li>
+		<li><a href ="/rules">Regeln</a></li>
+		<li><a href ="/galery">Galerie</a></li>
+		<li><a href ="/contact">Kontakt</a></li>
+
+	</ul>
+</nav>
+
+
+<slot />
+
+
+<style>
+ul{
+	display:flex;
+	list-style: none;
+	margin: 0;
+  	padding: 0;
+}
+li{
+	margin-right: 1em;
+	font-weight: bold;
+
+}
+
+li:hover{
+	text-decoration: underline;
+}
+</style>
