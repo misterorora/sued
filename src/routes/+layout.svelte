ae4c7d9c4cecb8acf4235225bb281e55faf23acc
Christians IdeaPad <cs.christian.steiner@gmail.com>
2024-03-10 14:31:13 +0100

    addded transitions, about to add markdown stuff!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,24 @@
 <title>Südstadt-Volleyball</title>
 <script>
-    import Header from './header.svelte';
-    import Footer from './footer.svelte';
-    import './global.css';
+    import { fade } from 'svelte/transition';   /*used for transitions at SPA´s */
+    import { cubicIn, cubicOut } from 'svelte/easing';
+    export let data;
+    import Header from './header.svelte';       /*imported for header on all Pages */
+    import Footer from './footer.svelte';       /*imported for footer on all Pages */
+    import './global.css';                      /*imported for svg background and fonts on all Pages */
 </script>
 
 <main>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <Header />
-    <slot />
     <Footer />
-
+    
+    <!-- Transition -->
+    {#key data.pathname}
+    <div
+        in:fade={{ easing: cubicOut, duration: 300, delay: 400 }}
+        out:fade={{ easing: cubicIn, duration: 300 }}
+    >
+        <slot />
+    </div>
+{/key}
 </main>
